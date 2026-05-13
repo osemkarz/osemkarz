1add6c37bda2a11d786e88751f21acb98527b508
osemkarz <zalicznafarcie@gmail.com>
2026-05-13 19:42:01 +0200

    Create page.js

---
+++ app/kurs/rownania-liniowe/page.js
@@ -0,0 +1,10 @@
+import RownaniaTiLesson from '../../../components/lessons/RownaniaTiLesson'
+
+export const metadata = {
+  title: 'Równania liniowe z jedną niewiadomą — Ósemkarz',
+  description: 'Kompletna lekcja o równaniach liniowych. Teoria, quiz, fiszki i kartkówka.',
+}
+
+export default function RownaniaTiPage() {
+  return <RownaniaTiLesson />
+}
