c254d9c5481cd5c5d0e4588653db2d548e37467b
osemkarz <zalicznafarcie@gmail.com>
2026-05-13 18:43:38 +0200

    Update page.js

--- app/kurs/page.js
+++ app/kurs/page.js
@@ -123,7 +123,7 @@ export default function KursPage() {
                     </div>
                   </div>
                   {!locked && (
-                    <Link href={d.status === 'active' ? d.n === '03' ? '/kurs/nierownosci' : '#' : '#'} style={{
+                    <Link href={!locked ? `/kurs/dzial-${d.n.replace('0','')}` : '#'} style={{
                       padding: '8px 18px', fontSize: 12, fontWeight: 500, borderRadius: 8,
                       background: d.status === 'active' ? '#F5541E' : d.status === 'done' ? 'transparent' : 'transparent',
                       color: d.status === 'active' ? '#fff' : d.status === 'done' ? '#00B894' : '#8896A5',
