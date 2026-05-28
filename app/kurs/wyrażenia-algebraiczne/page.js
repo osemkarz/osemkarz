41f79e73ba4543f7567d332eb92043b5f7e231f4
osemkarz <zalicznafarcie@gmail.com>
2026-05-28 16:01:59 +0200

    Create page.js

---
+++ app/kurs/wyrażenia-algebraiczne/page.js
@@ -0,0 +1,3 @@
+import WyrazeniaAlgebraiczneLesson from '../../../components/lessons/WyrazeniaAlgebraiczneLesson'
+export const metadata = { title: 'Wyrażenia algebraiczne — Ósemkarz' }
+export default function Page() { return <WyrazeniaAlgebraiczneLesson /> }
