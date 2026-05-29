92e1a22eb9b7fdc00dc7ecac71d454308801bc4a
osemkarz <zalicznafarcie@gmail.com>
2026-05-29 09:46:59 +0200

    Delete app/kurs/wyrażenia-algebraiczne/page.js

--- app/kurs/wyrażenia-algebraiczne/page.js
+++
@@ -1,3 +0,0 @@
-import WyrazeniaAlgebraiczneLesson from '../../../components/lessons/WyrazeniaAlgebraiczneLesson'
-export const metadata = { title: 'Wyrażenia algebraiczne — Ósemkarz' }
-export default function Page() { return <WyrazeniaAlgebraiczneLesson /> }
