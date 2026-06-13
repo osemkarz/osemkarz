1ecfecd5b486164ce3e7d7e1da13353dd3b7e000
osemkarz <zalicznafarcie@gmail.com>
2026-06-13 13:16:07 +0200

    Create page.js

---
+++ app/kurs/zmienne-wyrazenia/page.js
@@ -0,0 +1,3 @@
+import ZmienneWyrazeniaLesson from '../../../components/lessons/ZmienneWyrazeniaLesson'
+export const metadata = { title: 'Zmienne i wyrazenia — Osemkarz' }
+export default function Page() { return <ZmienneWyrazeniaLesson /> }
