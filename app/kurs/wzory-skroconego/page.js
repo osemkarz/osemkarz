d4411e14829f9a7fafb83dc134720abe4aaad437
osemkarz <zalicznafarcie@gmail.com>
2026-06-16 18:42:29 +0200

    Create page.js

---
+++ app/kurs/wzory-skroconego/page.js
@@ -0,0 +1,3 @@
+import WzorySkroconegMnozenia from '../../../components/lessons/WzorySkroconegMnozenia'
+export const metadata = { title: 'Wzory skróconego mnożenia — Ósemkarz' }
+export default function Page() { return <WzorySkroconegMnozenia /> }
