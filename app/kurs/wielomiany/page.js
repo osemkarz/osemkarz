b3c13e564514bb7f152ef4cd4484e797c08bf8fc
osemkarz <zalicznafarcie@gmail.com>
2026-06-16 19:22:01 +0200

    Create page.js

---
+++ app/kurs/wielomiany/page.js
@@ -0,0 +1,3 @@
+import WielomianyLesson from '../../../components/lessons/WielomianyLesson'
+export const metadata = { title: 'Działania na wielomianach — Ósemkarz' }
+export default function Page() { return <WielomianyLesson /> }
